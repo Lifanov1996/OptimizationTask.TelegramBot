2b86f6b76b2c9a20ae159e9c3bfbb6c98fff26c8
Evgeny Lifanov <lifanov.j@yandex.ru>
2023-03-16 18:23:21 +0300

    Создание решения

---
+++ TelegramBot/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types;
+
+var client = new TelegramBotClient("6185901925:AAE5qxhMueMwahHhJmgr2-jsS3syBqgBTok");
+
+client.StartReceiving(HandleUpdateAsync, HandlePollingErrorAsync);
+Console.ReadKey();
+
+async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken token)
+{
+    var message = update.Message;
+    if(message.Text != null)
+    {
+        Console.WriteLine($"{DateTime.Now.ToString("G")} | {message.Chat.FirstName}");
+
+        if (message.Text.ToLower().Trim().Contains("привет"))
+        {
+            await botClient.SendTextMessageAsync(message.Chat.Id, "Ну привет");
+            return;
+        }
+    }
+}
+
+Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception ex, CancellationToken token)
+{
+    var ErrorMessage = ex switch
+    {
+        ApiRequestException apiRequestException
+            => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",_ => ex.ToString()
+    };
+
+    Console.WriteLine(ErrorMessage);
+    return Task.CompletedTask;
+}
