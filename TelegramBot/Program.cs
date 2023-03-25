f9b6adbeec3cbe550e5211f55893d81ea261bfe4
Evgeny Lifanov <lifanov.j@yandex.ru>
2023-03-25 12:36:37 +0300

    Обработка исключений

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -107,14 +107,24 @@ async Task PostApplicationAsync(ITelegramBotClient botClient, Message message)
 async Task GetProjectMessage(ITelegramBotClient botClient, Message message)
 {
     Projects project = new(new ResponseServices());
-    var projectsResponse = await project.GetProjectsAsync();
-    foreach (var item in projectsResponse)
+    try
     {
-        await botClient.SendTextMessageAsync(message.Chat.Id, 
-                                            $"\n{item.File}" +
-                                            $"<strong>{item.Header}<strong>" +
-                                            $"\n{item.Description}",
-                                            parseMode: ParseMode.Html);
+        var projectsResponse = await project.GetProjectsAsync();
+        foreach (var item in projectsResponse)
+        {
+            await botClient.SendTextMessageAsync(message.Chat.Id, 
+                                                $"\n{item.File}" +
+                                                $"<strong>{item.Header}<strong>" +
+                                                $"\n{item.Description}",
+                                                parseMode: ParseMode.Html);
+        }
+    }
+    catch(Exception ex)
+    {
+        await botClient.SendTextMessageAsync(message.Chat.Id,
+                                                $"<strong>Ошибка загрузки сервиса</strong>" +
+                                                $"\nНе удалось загрузить данные",
+                                                parseMode: ParseMode.Html);
     }
 }
 
@@ -122,13 +132,23 @@ async Task GetProjectMessage(ITelegramBotClient botClient, Message message)
 async Task GetOfficeAsync(ITelegramBotClient botClient, Message message)
 {
     Offices office = new(new ResponseServices());
-    var officesResponse = await office.GetOfficeAsync();
-    foreach (var item in officesResponse)
+    try
+    {
+        var officesResponse = await office.GetOfficeAsync();
+        foreach (var item in officesResponse)
+        {
+            await botClient.SendTextMessageAsync(message.Chat.Id,
+                                                $"<strong>{item.header}</strong>" +
+                                                $"\n{item.description}",
+                                                parseMode: ParseMode.Html);
+        }
+    }
+    catch(Exception ex)
     {
         await botClient.SendTextMessageAsync(message.Chat.Id,
-                                            $"<strong>{item.header}</strong>" +
-                                            $"\n{item.description}",
-                                            parseMode: ParseMode.Html);
+                                                $"<strong>Ошибка загрузки сервиса</strong>" +
+                                                $"\nНе удалось загрузить данные",
+                                                parseMode: ParseMode.Html);
     }
 }
 
@@ -136,15 +156,25 @@ async Task GetOfficeAsync(ITelegramBotClient botClient, Message message)
 async Task GetTidingAsync(ITelegramBotClient botClient, Message message)
 {
     Tidings tiding = new(new ResponseServices());
-    var tidingsResponse = await tiding.GetTidingsAsync();
-    foreach (var item in tidingsResponse)
+    try
+    {
+        var tidingsResponse = await tiding.GetTidingsAsync();
+        foreach (var item in tidingsResponse)
+        {
+            await botClient.SendTextMessageAsync(message.Chat.Id,
+                                                $"{item.File}" +
+                                                $"\nОпубликлванно - {item.DateTimePublication.ToString("G")}" +
+                                                $"\n<strong>{item.Header}</strong>" +
+                                                $"\n{item.Description}",
+                                                parseMode: ParseMode.Html);
+        }
+    }
+    catch(Exception ex)
     {
         await botClient.SendTextMessageAsync(message.Chat.Id,
-                                            $"{item.File}" +
-                                            $"\nОпубликлванно - {item.DateTimePublication.ToString("G")}" +
-                                            $"\n<strong>{item.Header}</strong>" +
-                                            $"\n{item.Description}",
-                                            parseMode: ParseMode.Html);
+                                                $"<strong>Ошибка загрузки сервиса</strong>" +
+                                                $"\nНе удалось загрузить данные",
+                                                parseMode: ParseMode.Html);
     }
 }
 
@@ -152,21 +182,31 @@ async Task GetTidingAsync(ITelegramBotClient botClient, Message message)
 async Task GetContactAsync(ITelegramBotClient botClient, Message message)
 {
     Contacts contact = new(new ResponseServices());
-    var contactResponse = await contact.GetContactAsync();
+    try
+    {
+        var contactResponse = await contact.GetContactAsync();
 
-    await botClient.SendContactAsync(message.Chat.Id,
-                                        phoneNumber: $"{contactResponse.CompanyNumber}",
-                                        firstName: "Mir");
+        await botClient.SendContactAsync(message.Chat.Id,
+                                            phoneNumber: $"{contactResponse.CompanyNumber}",
+                                            firstName: "Mir");
 
-    await botClient.SendTextMessageAsync(message.Chat.Id,
-                                        $"<strong>Email:</strong> {contactResponse.CompanyEmail}",
-                                        parseMode: ParseMode.Html);
+        await botClient.SendTextMessageAsync(message.Chat.Id,
+                                            $"<strong>Email:</strong> {contactResponse.CompanyEmail}",
+                                            parseMode: ParseMode.Html);
 
-    await botClient.SendVenueAsync(message.Chat.Id,
-                                    latitude: 55.155659,
-                                    longitude: 37.459864,
-                                    title: "Адрес компании",
-                                    address: $"{contactResponse.CompanyAdress}");
+        await botClient.SendVenueAsync(message.Chat.Id,
+                                        latitude: 55.155659,
+                                        longitude: 37.459864,
+                                        title: "Адрес компании",
+                                        address: $"{contactResponse.CompanyAdress}");
+    }
+    catch(Exception ex)
+    {
+        await botClient.SendTextMessageAsync(message.Chat.Id,
+                                                $"<strong>Ошибка загрузки сервиса</strong>" +
+                                                $"\nНе удалось загрузить данные",
+                                                parseMode: ParseMode.Html);
+    }
 }
 
 //Переход на сайт компании
